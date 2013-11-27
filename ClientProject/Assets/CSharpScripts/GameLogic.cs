9dcc9cd5f6e7da537fdd75b754564d7c3dcd0478
liuming <lmas19820607@hotmail.com>
2013-11-27 22:25:52 +0800

    炸弹炸两圈

--- ClientProject/Assets/CSharpScripts/GameLogic.cs
+++ ClientProject/Assets/CSharpScripts/GameLogic.cs
@@ -707,8 +707,6 @@ public class GameLogic
         //   }
         //}
 
-        Color defaultColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-
         //根据数据绘制Sprite
         for (int i = 0; i < BlockCountX; i++)
         {
@@ -731,7 +729,6 @@ public class GameLogic
                     }
 
                     m_blocks[i, j].m_blockTransform.localPosition = new Vector3(GetXPos(i) + m_blocks[i, j].x_move, -(m_blocks[i, j].y_move + GetYPos(i, j)), -105);
-                    //m_blocks[i, j].m_blockSprite.color = defaultColor;          //Todo 实在不知道为什么加上这句动画控制Alpha才好使
 
                     if (m_blocks[i, j].IsEating())
                     {
@@ -2029,9 +2026,14 @@ public class GameLogic
                 {
                     for (TDirection dir = TDirection.EDir_Up; dir <= TDirection.EDir_LeftUp; ++dir)
                     {
-                        EatBlock(GoTo(position, dir, 1), CapsConfig.BombEffectInterval, 50);
+                        Position newPos = GoTo(position, dir, 1);                            //第一层
+                        EatBlock(newPos, CapsConfig.BombEffectInterval, 50);
 
-                        EatBlock(GoTo(position, dir, 2), CapsConfig.BombEffectInterval * 2, 50);
+                        newPos = GoTo(newPos, dir, 1);                                     //第二层
+                        EatBlock(newPos, CapsConfig.BombEffectInterval * 2, 50);
+
+                        newPos = GoTo(newPos, (TDirection)(((int)dir + 2) % 6), 1);     //第二层向下一个方向走一步
+                        EatBlock(newPos, CapsConfig.BombEffectInterval * 2, 50);
                     }
                     AddPartile("BombEffect", position.x, position.y);
                 }

9e848c23e79894e4822657037822864e61ce5f1e
liuming <lmas19820607@hotmail.com>
2013-12-13 23:20:12 +0800

    声音文件

---
+++ ClientProject/Assets/CSharpScripts/AudioList.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioList : MonoBehaviour {
+	
+	public AudioClip BombAudioClip;
+    public AudioClip DropClip;
+    public AudioClip EatClip;
+    public AudioClip EatColorClip;
+    public AudioClip ItemBirthClip;
+    public AudioClip JellyClip;
+    public AudioClip Line1Clip;
+    public AudioClip MoveFailedClip;
+    public AudioClip StoneClip;
+	public AudioClip CageClip;
+	public AudioClip ChocolateClip;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

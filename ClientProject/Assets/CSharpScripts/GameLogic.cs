847f5559a5e6026f03f9d58bca6d1b822c411245
liuming <lmas19820607@hotmail.com>
2013-12-17 09:16:23 +0800

    微调游戏区坐标

--- ClientProject/Assets/CSharpScripts/GameLogic.cs
+++ ClientProject/Assets/CSharpScripts/GameLogic.cs
@@ -436,7 +436,7 @@ public class GameLogic
         }
 
         gameAreaX = (int)((CapsApplication.Singleton.Width - (BlockXEnd + 1 - BlockXStart) * BLOCKWIDTH) / 2 - BlockXStart * BLOCKWIDTH);
-        gameAreaY = (int)((CapsApplication.Singleton.Height - (BlockYEnd + 1 - BlockYStart) * BLOCKHEIGHT) / 2 - BlockYStart * BLOCKHEIGHT - BLOCKHEIGHT /2);
+        gameAreaY = (int)((CapsApplication.Singleton.Height - (BlockYEnd + 1 - BlockYStart) * BLOCKHEIGHT) / 2 - BlockYStart * BLOCKHEIGHT - BLOCKHEIGHT /2) - 10;
 
         //绘制底图
         for (int i = 0; i < BlockCountX; ++i)

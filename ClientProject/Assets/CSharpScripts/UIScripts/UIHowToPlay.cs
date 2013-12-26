47baab77da53d7f5a349cf0afb26daeef3e5089b
liuming <lmas19820607@hotmail.com>
2013-12-27 00:08:01 +0800

    帮助界面的基本功能

--- ClientProject/Assets/CSharpScripts/UIScripts/UIHowToPlay.cs
+++ ClientProject/Assets/CSharpScripts/UIScripts/UIHowToPlay.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class UIHowToPlay : UIWindow 
 {
+    int m_curPicNum;
+    UITexture m_helpTex;
+
     public override void OnCreate()
     {
         base.OnCreate();
+
+        m_helpTex = GetChildComponent<UITexture>("HelpPic");
+
         AddChildComponentMouseClick("CloseBtn", delegate()
         {
             HideWindow();
             UIWindowManager.Singleton.GetUIWindow<UIMainMenu>().ShowWindow();
         });
+		
+		AddChildComponentMouseClick("NextBtn", Next);
+    }
+
+    public override void OnShow()
+    {
+        base.OnShow();
+        m_curPicNum = 0;
+        Texture tex = ResourceManager.Singleton.GetIconByName("help" + m_curPicNum);
+        m_helpTex.mainTexture = tex;
+    }
+
+    void Next()
+    {
+        m_curPicNum = (m_curPicNum + 1)%3;
+        Texture tex = ResourceManager.Singleton.GetIconByName("help" + m_curPicNum);
+        m_helpTex.mainTexture = tex;
     }
 }
---
+++ ClientProject/Assets/CSharpScripts/UIScripts/UINoMoreHearts.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UINoMoreHearts : UIWindow 
+{
+    UIWindow m_mainMenuExtend;
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        AddChildComponentMouseClick("CloseBtn", Close);
+		AddChildComponentMouseClick("OKBtn", Close);
+
+        AddChildComponentMouseClick("Buy1HeartBtn", delegate()
+        {
+            if (GlobalVars.Coins > 0)
+            {
+                --GlobalVars.Coins;
+                ++GlobalVars.HeartCount;
+                PlayerPrefs.SetInt("Coins", GlobalVars.Coins);
+                ContinuePlay();
+            }
+        });
+
+        AddChildComponentMouseClick("Buy5HeartBtn", delegate()
+        {
+            if (GlobalVars.Coins >= 3)
+            {
+                GlobalVars.Coins -= 3;
+                GlobalVars.HeartCount = 5;
+                PlayerPrefs.SetInt("Coins", GlobalVars.Coins);
+                ContinuePlay();
+            }
+        });
+    }
+
+    void ContinuePlay()
+    {
+        GlobalVars.CurStageData.LoadStageData(GlobalVars.CurStageNum);
+        UIWindowManager.Singleton.GetUIWindow<UIStageInfo>().ShowWindow();
+        HideWindow();
+    }
+
+    void Close()
+	{
+		HideWindow();
+        if (CapsApplication.Singleton.CurStateEnum == StateEnum.Game)       //若在游戏中
+        {
+            CapsApplication.Singleton.ChangeState((int)StateEnum.Login);         //回大地图
+            UIWindowManager.Singleton.GetUIWindow<UIMainMenu>().ShowWindow();
+            UIWindowManager.Singleton.GetUIWindow<UIMap>().ShowWindow();        //返回地图，不需要刷新按钮
+            LoginState.Instance.CurFlow = TLoginFlow.LoginFlow_Map;         //切换流程到显示地图
+        }
+	}
+}

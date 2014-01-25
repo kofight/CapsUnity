8fa6cd4a95d2c5d83cf9ef0c9f551fb4931aa529
mysx <muyushuixin@126.com>
2014-01-25 14:16:58 +0800

    巧克力动画修改，巧克力声音修改，Ranbow目标特效修改得更内涵

--- ClientProject/Assets/CSharpScripts/CapsConfig.cs
+++ ClientProject/Assets/CSharpScripts/CapsConfig.cs
@@ -71,7 +71,7 @@ public class CapsConfig
     public static float EatLineEffectInterval = 0.04f;               //消行特效吃块的间隔
     public static float EatLineEffectStartInterval = 0.17f;          //消行特效吃块的开始时间
 
-    public static float EatAllLineEffectStartInterval = 0.37f;       //消行特效吃块的开始时间
+    public static float EatAllLineEffectStartInterval = 0.17f;       //消行特效吃块的开始时间
 
     public static float BombEffectInterval = 0.0f;                   //炸弹特效吃块的间隔
     public static float EatBombEffectStartInterval = 0.9f;           //炸弹特效吃块的开始时间

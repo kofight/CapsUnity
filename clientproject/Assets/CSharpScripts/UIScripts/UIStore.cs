45e1ef8f4980e7b593cf39a051eae4783ac8f4c1
liuming <lmas19820607@hotmail.com>
2014-02-17 23:18:25 +0800

    付费继续

--- clientproject/Assets/CSharpScripts/UIScripts/UIStore.cs
+++ clientproject/Assets/CSharpScripts/UIScripts/UIStore.cs
@@ -13,7 +13,51 @@ public class UIStore : UIWindow
         {
             HideWindow();
         });
+
+        AddChildComponentMouseClick("BuyItem1Btn", delegate()
+        {
+            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.140coins");
+        });
+        AddChildComponentMouseClick("BuyItem2Btn", delegate()
+        {
+            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.800coins");
+        });
+        AddChildComponentMouseClick("BuyItem3Btn", delegate()
+        {
+            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.1700coins");
+        });
+        AddChildComponentMouseClick("BuyItem4Btn", delegate()
+        {
+            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.4500coins");
+        });
+        AddChildComponentMouseClick("BuyItem5Btn", delegate()
+        {
+            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.9500coins");
+        });
+
+        Unibiller.onPurchaseComplete += OnPurchased;
+        Unibiller.onPurchaseFailed += OnPurchaseFailed;
+        Unibiller.onPurchaseCancelled += OnPurchaseCancelled;
     }
+
+    void OnPurchased(PurchasableItem item)      //购买成功
+    {
+        Debug.Log("Purchase Succeed");
+        HideWindow();
+    }
+
+    void OnPurchaseFailed(PurchasableItem item)     //购买失败
+    {
+        Debug.Log("Purchase Failed");
+        HideWindow();
+    }
+
+    void OnPurchaseCancelled(PurchasableItem item)      //主动取消购买
+    {
+        Debug.Log("Purchase Cancelled");
+        HideWindow();
+    }
+
     public override void OnShow()
     {
         base.OnShow();

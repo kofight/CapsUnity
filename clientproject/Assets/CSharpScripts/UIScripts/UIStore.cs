4f9ce6789803b11939e96ea90777cbe183392066
liuming <lmas19820607@hotmail.com>
2014-02-19 00:38:34 +0800

    补充提交

--- clientproject/Assets/CSharpScripts/UIScripts/UIStore.cs
+++ clientproject/Assets/CSharpScripts/UIScripts/UIStore.cs
@@ -16,23 +16,48 @@ public class UIStore : UIWindow
 
         AddChildComponentMouseClick("BuyItem1Btn", delegate()
         {
-            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.140coins");
+            HideWindow();
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().ShowWindow(delegate()
+            {
+                Unibiller.initiatePurchase("com.linkrstudio.jellycraft.140coins");
+            });
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().SetString(Localization.instance.Get("WaitForPurchase"));
         });
         AddChildComponentMouseClick("BuyItem2Btn", delegate()
         {
-            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.800coins");
+            HideWindow();
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().ShowWindow(delegate()
+            {
+                Unibiller.initiatePurchase("com.linkrstudio.jellycraft.800coins");
+            });
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().SetString(Localization.instance.Get("WaitForPurchase"));
         });
         AddChildComponentMouseClick("BuyItem3Btn", delegate()
         {
-            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.1700coins");
+            HideWindow();
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().ShowWindow(delegate()
+            {
+                Unibiller.initiatePurchase("com.linkrstudio.jellycraft.1700coins");
+            });
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().SetString(Localization.instance.Get("WaitForPurchase"));
         });
         AddChildComponentMouseClick("BuyItem4Btn", delegate()
         {
-            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.4500coins");
+            HideWindow();
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().ShowWindow(delegate()
+            {
+                Unibiller.initiatePurchase("com.linkrstudio.jellycraft.4500coins");
+            });
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().SetString(Localization.instance.Get("WaitForPurchase"));
         });
         AddChildComponentMouseClick("BuyItem5Btn", delegate()
         {
-            Unibiller.initiatePurchase("com.linkrstudio.jellycraft.9500coins");
+            HideWindow();
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().ShowWindow(delegate()
+            {
+                Unibiller.initiatePurchase("com.linkrstudio.jellycraft.9500coins");
+            });
+            UIWindowManager.Singleton.GetUIWindow<UIWait>().SetString(Localization.instance.Get("WaitForPurchase"));
         });
 
         Unibiller.onPurchaseComplete += OnPurchased;
@@ -43,19 +68,25 @@ public class UIStore : UIWindow
     void OnPurchased(PurchasableItem item)      //购买成功
     {
         Debug.Log("Purchase Succeed");
-        HideWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIWait>().HideWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIMessageBox>().ShowWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIMessageBox>().SetString(Localization.instance.Get("PurchaseSucceed"));
     }
 
     void OnPurchaseFailed(PurchasableItem item)     //购买失败
     {
         Debug.Log("Purchase Failed");
-        HideWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIWait>().HideWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIMessageBox>().ShowWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIMessageBox>().SetString(Localization.instance.Get("PurchaseFailed"));
     }
 
     void OnPurchaseCancelled(PurchasableItem item)      //主动取消购买
     {
         Debug.Log("Purchase Cancelled");
-        HideWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIWait>().HideWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIMessageBox>().ShowWindow();
+        UIWindowManager.Singleton.GetUIWindow<UIMessageBox>().SetString(Localization.instance.Get("PurchaseCancelled"));
     }
 
     public override void OnShow()

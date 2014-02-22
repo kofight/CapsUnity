02c832249eb2b38baaa8469f71dcc306217385e3
liuming <lmas19820607@hotmail.com>
2014-02-22 21:17:56 +0800

    修正: 第9关FTUE过不去 补充提交使用道具的修改

--- clientproject/Assets/CSharpScripts/UIScripts/UIPurchaseNoTarget.cs
+++ clientproject/Assets/CSharpScripts/UIScripts/UIPurchaseNoTarget.cs
@@ -21,7 +21,7 @@ public class UIPurchaseNoTarget : UIWindow
     public override void OnShow()
     {
         base.OnShow();
-        GameLogic.Singleton.PauseGame();
+        //GameLogic.Singleton.PauseGame();
 		if(GlobalVars.UsingItem == PurchasedItem.Item_Hammer)
 		{
 			
@@ -37,20 +37,34 @@ public class UIPurchaseNoTarget : UIWindow
     {
         HideWindow(delegate()
         {
-            GameLogic.Singleton.ResumeGame();
+            //GameLogic.Singleton.ResumeGame();
         });
-		OnPurchase();
+        if (GlobalVars.UsingItem == PurchasedItem.Item_PlusStep)
+        {
+            if (Unibiller.DebitBalance("gold", 6))      //花钱
+            {
+                GA.API.Business.NewEvent("BuyStep", "RMB", 1);
+                PlayerPrefs.SetInt("Coins", GlobalVars.Coins);
+                if (GlobalVars.CurStageData.StepLimit > 0)
+                    GameLogic.Singleton.PlayingStageData.StepLimit += 5;        //步数加5
+                else if (GlobalVars.CurStageData.TimeLimit > 0)
+                    GameLogic.Singleton.AddGameTime(15);        //Add 15 Seconds
+            }
+        }
     }
 
     public void OnCancelClicked()
     {
         HideWindow(delegate()
         {
-            GameLogic.Singleton.ResumeGame();
+            //GameLogic.Singleton.ResumeGame();
         });
-        if (GameLogic.Singleton.GetGameFlow() == TGameFlow.EGameState_End)
+        if (GlobalVars.UsingItem == PurchasedItem.Item_PlusStep)
         {
-            UIWindowManager.Singleton.GetUIWindow<UIGameEnd>().ShowWindow();
+            if (GameLogic.Singleton.GetGameFlow() == TGameFlow.EGameState_End)
+            {
+                UIWindowManager.Singleton.GetUIWindow<UIGameEnd>().ShowWindow();
+            }
         }
     }
 }

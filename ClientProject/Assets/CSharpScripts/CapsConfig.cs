fdf4792eb5e9ca7a8b5f97b006c4457178781720
mysx <muyushuixin@126.com>
2014-03-12 23:37:45 +0800

    重拍时间修改

--- ClientProject/Assets/CSharpScripts/CapsConfig.cs
+++ ClientProject/Assets/CSharpScripts/CapsConfig.cs
@@ -56,7 +56,7 @@ public class CapsConfig
     public static readonly int EffectBigBombTime = 2000;
     public static readonly int EffectEatAColorNDBombTime = 2000;
     public static readonly int EffectResortTime = 600;                            //重拍特效中间的时间
-    public static readonly int EffectResortInterval = 30;                         //重拍特效的时间间隔
+    public static readonly int EffectResortInterval = 15;                         //重拍特效的时间间隔
 
     public static readonly string EatEffect = "EatEffect";                      //吃块的目标特效
     public static readonly string BombEatEffect = "BombEatEffect";              //炸弹吃块的目标特效

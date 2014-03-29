09f9b3adc8db0ec7740445bb691f2dd006e1146b
liuming <lmas19820607@hotmail.com>
2014-03-29 14:29:15 +0800

    补充提交

---
+++ clientproject/Assets/CSharpScripts/S5Lib/ToolKits/PressReceiver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressReceiver : MonoBehaviour {
+	
+	public UIWindow.WindowEffectFinished OnPressFunc;
+	
+	public void OnPress (bool isPressed)
+	{
+		if(OnPressFunc != null)
+		{
+			OnPressFunc();
+		}
+	}
+}
